16aee23a979a33075589f3213f48fd5e632a8d77
Alex Moreton <lxmrtn7@gmail.com>
2024-10-04 14:54:46 +0700

    add safe area for native

--- expo-next/app/_layout.tsx
+++ expo-next/app/_layout.tsx
@@ -1,12 +1,15 @@
 import "./global.css";
 import "maplibre-gl/dist/maplibre-gl.css";
+import { SafeAreaView } from "react-native-safe-area-context";
 
 import { Stack } from "expo-router";
 
 export default function RootLayout() {
   return (
-    <Stack>
-      <Stack.Screen name="index" />
-    </Stack>
+    <SafeAreaView style={{ flex: 1 }}>
+      <Stack>
+        <Stack.Screen name="index" />
+      </Stack>
+    </SafeAreaView>
   );
 }
