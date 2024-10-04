92acebbfc8db839654318e70000522b0e267b1a6
Alex Moreton <lxmrtn7@gmail.com>
2024-10-04 14:35:46 +0700

    add barebones maplibre web implementation with local json

--- expo-next/app/search/index.tsx
+++ expo-next/app/search/index.tsx
@@ -1,16 +1,13 @@
 // src/App.js
 // @ts-nocheck
-import { View, Text } from "react-native";
+import { View } from "react-native";
 import React from "react";
 import MapLibreMap from "../../components/mapLibre";
-// import MapboxMap from "../../components/mapbox/index";
 
 function App() {
   return (
     <View className="flex-1">
-      <Text>MapLibre GL JS Uninversal Map</Text>
       <MapLibreMap />
-      {/* <MapboxMap /> */}
     </View>
   );
 }
