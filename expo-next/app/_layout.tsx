2a4da98543eed56bb2a1ff96d670432e20c92c58
Alex Moreton <lxmrtn7@gmail.com>
2024-10-06 13:00:39 +0700

    initial implementation of state mgmt with zustand and react-query

--- expo-next/app/_layout.tsx
+++ expo-next/app/_layout.tsx
@@ -1,15 +1,19 @@
 import "./global.css";
 import "maplibre-gl/dist/maplibre-gl.css";
 import { SafeAreaView } from "react-native-safe-area-context";
-
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { Stack } from "expo-router";
+
+const queryClient = new QueryClient();
 
 export default function RootLayout() {
   return (
-    <SafeAreaView style={{ flex: 1 }}>
-      <Stack>
-        <Stack.Screen name="index" />
-      </Stack>
-    </SafeAreaView>
+    <QueryClientProvider client={queryClient}>
+      <SafeAreaView style={{ flex: 1 }}>
+        <Stack>
+          <Stack.Screen name="index" />
+        </Stack>
+      </SafeAreaView>
+    </QueryClientProvider>
   );
 }
