9ef393dcd1aaec50f51e57440349d6139fc75182
Alex Moreton <lxmrtn7@gmail.com>
2024-10-19 14:27:56 +0700

    shelved for now

--- sst.config.ts
+++ sst.config.ts
@@ -1,14 +1,40 @@
-import { SSTConfig } from "sst";
-import { ExampleStack } from "./stacks/ExampleStack";
+/// <reference path="./.sst/platform/config.d.ts" />
 
-export default {
-  config(_input) {
+export default $config({
+  app(input) {
     return {
-      name: "green-one-auth",
-      region: "us-east-1",
+      name: "green-me",
+      removal: input?.stage === "production" ? "retain" : "remove",
+      home: "aws",
     };
   },
-  stacks(app) {
-    app.stack(ExampleStack);
-  }
-} satisfies SSTConfig;
+  async run() {
+    const api = new sst.aws.ApiGatewayV2("api");
+
+    api.route("GET /session", {
+      handler: "packages/functions/src/session.handler",
+    });
+
+    // "GET /profiles": "packages/functions/src/profiles/list.handler",
+    // "POST /profiles": "packages/functions/src/profiles/create.handler",
+    // "GET /venues": "packages/functions/src/venues/list.handler",
+    // "POST /venues": "packages/functions/src/venues/create.handler",
+
+    //     defaults: {
+    //       function: {
+    //         runtime: "nodejs18.x",
+
+    //         // bind - injects the environment variables directly into the Lambda function
+    //         // at runtime and ensures the function has the necessary permissions
+    //         bind: [
+    //           userDetailsFromAuthProvider,
+    //           profilesTable,
+    //           venuesTable,
+    //           chatsTable,
+    //           messagesTable,
+    //         ],
+    //       },
+    //     },
+    // });
+  },
+});
--- stacks/__ExampleStack.ts
+++ stacks/__ExampleStack.ts
@@ -1,22 +1,15 @@
-import {
-  StackContext,
-  Api,
-  Auth,
-  StaticSite,
-  Table,
-  Config,
-} from "sst/constructs";
+import sst from "sst";
 
 export function ExampleStack({ app, stack }: StackContext) {
   // Simple details from 3rd party Auth providers
-  const userDetailsFromAuthProvider = new Table(stack, "users", {
+  const userDetailsFromAuthProvider = new sst.aws.Table(stack, "users", {
     fields: {
       userId: "string",
     },
     primaryIndex: { partitionKey: "userId" },
   });
 
-  // const userSettings = new Table(stack, "UsersTable", {
+  // const userSettings = new sst.aws.Table(stack, "UsersTable", {
   //   fields: {
   //     id: "string",
   //     email: "string",
@@ -29,20 +22,20 @@ export function ExampleStack({ app, stack }: StackContext) {
 
   const stage = app.local ? "greenmev2" : "prod";
 
-  new Config.Parameter(stack, "PROFILES_TABLE", {
+  new sst.aws.Config.Parameter(stack, "PROFILES_TABLE", {
     value: `${stage}-green-one-auth-ProfilesTable`,
   });
-  new Config.Parameter(stack, "VENUES_TABLE", {
+  new sst.aws.Config.Parameter(stack, "VENUES_TABLE", {
     value: `${stage}-green-one-auth-VenuesTable`,
   });
-  new Config.Parameter(stack, "CHATS_TABLE", {
+  new sst.aws.Config.Parameter(stack, "CHATS_TABLE", {
     value: `${stage}-green-one-auth-ChatsTable`,
   });
-  new Config.Parameter(stack, "MESSAGES_TABLE", {
+  new sst.aws.Config.Parameter(stack, "MESSAGES_TABLE", {
     value: `${stage}-green-one-auth-MessagesTable`,
   });
 
-  const profilesTable = new Table(stack, "ProfilesTable", {
+  const profilesTable = new sst.aws.Table(stack, "ProfilesTable", {
     fields: {
       user_id: "string",
       id: "string",
@@ -54,7 +47,7 @@ export function ExampleStack({ app, stack }: StackContext) {
     primaryIndex: { partitionKey: "user_id", sortKey: "id" },
   });
 
-  const venuesTable = new Table(stack, "VenuesTable", {
+  const venuesTable = new sst.aws.Table(stack, "VenuesTable", {
     fields: {
       user_id: "string",
       id: "string",
@@ -66,7 +59,7 @@ export function ExampleStack({ app, stack }: StackContext) {
     primaryIndex: { partitionKey: "user_id", sortKey: "id" },
   });
 
-  const chatsTable = new Table(stack, "ChatsTable", {
+  const chatsTable = new sst.aws.Table(stack, "ChatsTable", {
     fields: {
       venue_id: "string",
       id: "string",
@@ -80,7 +73,7 @@ export function ExampleStack({ app, stack }: StackContext) {
     },
   });
 
-  const messagesTable = new Table(stack, "MessagesTable", {
+  const messagesTable = new sst.aws.Table(stack, "MessagesTable", {
     fields: {
       chat_id: "string",
       sent_at: "string",
@@ -91,36 +84,10 @@ export function ExampleStack({ app, stack }: StackContext) {
   });
 
   // Create Api
-  const api = new Api(stack, "api", {
-    defaults: {
-      function: {
-        runtime: "nodejs18.x",
-
-        // bind - injects the environment variables directly into the Lambda function
-        // at runtime and ensures the function has the necessary permissions
-        bind: [
-          userDetailsFromAuthProvider,
-          profilesTable,
-          venuesTable,
-          chatsTable,
-          messagesTable,
-        ],
-      },
-    },
-    routes: {
-      "GET /session": "packages/functions/src/session.handler",
-      "GET /profiles": "packages/functions/src/profiles/list.handler",
-      "POST /profiles": "packages/functions/src/profiles/create.handler",
-      "GET /venues": "packages/functions/src/venues/list.handler",
-      "POST /venues": "packages/functions/src/venues/create.handler",
-      // "POST /chat/message": "packages/functions/src/chat/sendMessage.handler",
-      // "GET /chat/{chatId}/messages":
-      //   "packages/functions/src/chat/getMessages.handler",
-    },
-  });
+  
 
   // Create a static site
-  const site = new StaticSite(stack, "site", {
+  const site = new sst.aws.StaticSite(stack, "site", {
     path: "expo-next",
     // no generated hash for mp4 asset in the build even tho metro is flagged to use mp4??
     buildCommand: "npm run build",
@@ -141,14 +108,14 @@ export function ExampleStack({ app, stack }: StackContext) {
   });
 
   // Create Auth provider
-  const auth = new Auth(stack, "auth", {
+  const auth = new sst.aws.Auth(stack, "auth", {
     authenticator: {
       handler: "packages/functions/src/auth.handler",
       bind: [
         site,
-        new Config.Secret(stack, "FACEBOOK_APP_ID"),
-        new Config.Secret(stack, "FACEBOOK_APP_SECRET"),
-        new Config.Secret(stack, "GOOGLE_CLIENT_ID"),
+        new sst.Secret("FACEBOOK_APP_ID"),
+        new sst.Secret("FACEBOOK_APP_SECRET"),
+        new sst.Secret("GOOGLE_CLIENT_ID"),
       ],
     },
   });
