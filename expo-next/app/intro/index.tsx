a4b4239fe2038deba53232a7c3e3aa876b7f4668
Alex Moreton <lxmrtn7@gmail.com>
2024-11-01 14:10:14 +0700

    fix stupid line dot android style

--- expo-next/app/intro/index.tsx
+++ expo-next/app/intro/index.tsx
@@ -53,71 +53,99 @@ const IntroScreen = () => {
             buying and selling items that are environmentally conscious. The
             greener your choices, the more tokens you will collect.
           </Text>
-          <Text className="text-[#111418] text-lg font-bold leading-tight tracking-[-0.015em] px-4 pb-2 pt-4">
+          <Text className="text-[#111418] text-lg font-bold leading-tight tracking-[-0.015em] px-8 pb-2 pt-4">
             How it works
           </Text>
-          <View className="grid grid-cols-[40px_1fr] gap-x-2 px-4">
-            <View className="flex flex-col items-center gap-1 pt-5">
-              <View className="size-2 rounded-full bg-[#111418]" />
-              <View className="w-[1.5px] bg-[#dce0e5] h-4 grow" />
+          <View className="pt-2 pb-2 pr-8 pl-8">
+            <View className="flex flex-row items-start gap-x-2 w-full">
+              {/* Left Side: Dot and Line */}
+              <View className="flex items-center">
+                <View className="w-[1.5px] h-4 " />
+                <View className="w-2 h-2 rounded-full bg-[#111418] my-1" />
+                <View className="w-[1.5px] flex-1 bg-[#dce0e5]" />
+              </View>
+
+              {/* Right Side: Text */}
+              <View className="flex-1 pt-3 pb-5">
+                <Text className="text-[#111418] text-base font-medium leading-normal">
+                  A breakdown of token reward is shown during the filters and
+                  search and sent to your Metamask or other reputable and secure
+                  web3 wallet.
+                </Text>
+              </View>
             </View>
-            <View className="flex flex-1 flex-col pt-3 pb-5">
-              <Text className="text-[#111418] text-base font-medium leading-normal">
-                Search for local or online item
-              </Text>
+            <View className="flex flex-row items-start gap-x-2 w-full">
+              {/* Left Side: Dot and Line */}
+              <View className="flex items-center">
+                <View className="w-[1.5px] h-4 bg-[#dce0e5]" />
+                <View className="w-2 h-2 rounded-full bg-[#111418] my-1" />
+                <View className="w-[1.5px] flex-1 bg-[#dce0e5]" />
+              </View>
+
+              {/* Right Side: Text */}
+              <View className="flex-1 pt-3 pb-5">
+                <Text className="text-[#111418] text-base font-medium leading-normal">
+                  Once purchased/received this will be logged optionally at the
+                  users consent.
+                </Text>
+              </View>
             </View>
-            <View className="flex flex-col items-center gap-1">
-              <View className="w-[1.5px] bg-[#dce0e5] h-4" />
-              <View className="size-2 rounded-full bg-[#111418]" />
-              <View className="w-[1.5px] bg-[#dce0e5] h-4 grow" />
+            <View className="flex flex-row items-start gap-x-2 w-full">
+              {/* Left Side: Dot and Line */}
+              <View className="flex items-center">
+                <View className="w-[1.5px] h-4 bg-[#dce0e5]" />
+                <View className="w-2 h-2 rounded-full bg-[#111418] my-1" />
+                <View className="w-[1.5px] flex-1 bg-[#dce0e5]" />
+              </View>
+
+              {/* Right Side: Text */}
+              <View className="flex-1 pt-3 pb-5">
+                <Text className="text-[#111418] text-base font-medium leading-normal">
+                  A breakdown of token reward is shown during the filters and
+                  search and send to your Metamask or other reputable and secure
+                  web3 wallet
+                </Text>
+              </View>
             </View>
-            <View className="flex flex-1 flex-col pt-3 pb-5">
-              <Text className="text-[#111418] text-base font-medium leading-normal">
-                Once purchased/received this will be logged optionally at the
-                users consent.
-              </Text>
+            <View className="flex flex-row items-start gap-x-2 w-full">
+              {/* Left Side: Dot and Line */}
+              <View className="flex items-center">
+                <View className="w-[1.5px] h-4 bg-[#dce0e5]" />
+                <View className="w-2 h-2 rounded-full bg-[#111418] my-1" />
+                <View className="w-[1.5px] flex-1 bg-[#dce0e5]" />
+              </View>
+
+              {/* Right Side: Text */}
+              <View className="flex-1 pt-3 pb-5">
+                <Text className="text-[#111418] text-base font-medium leading-normal">
+                  This token has a 1:1 mapping of the amount of Co2 emissions
+                  tons that we can get away with to stay under the 2 degrees
+                  global warming by 2050. Current estimates mean there are 1
+                  trillion tokens : 1 trillion mega tons of emissions to track
+                  and reduce via the insuance of this token, similar to carbon
+                  credits. The more tokens you earn, the greater impact we may
+                  have on this seemingly impossible feat
+                </Text>
+              </View>
             </View>
-            <View className="flex flex-col items-center gap-1 pb-3">
-              <View className="w-[1.5px] bg-[#dce0e5] h-4" />
-              <View className="size-2 rounded-full bg-[#111418]" />
-              <View className="w-[1.5px] bg-[#dce0e5] h-full" />
-            </View>
-            <View className="flex flex-1 flex-col pt-3 pb-5">
-              <Text className="text-[#111418] text-base font-medium leading-normal">
-                A breakdown of token reward is shown during the filters and
-                search and send to your Metamask or other reputable and secure
-                web3 wallet
-              </Text>
-            </View>
-            <View className="flex flex-col items-center gap-1 pb-3">
-              <View className="w-[1.5px] bg-[#dce0e5] h-4" />
-              <View className="size-2 rounded-full bg-[#111418]" />
-              <View className="w-[1.5px] bg-[#dce0e5] h-full" />
-            </View>
-            <View className="flex flex-1 flex-col pt-3 pb-5">
-              <Text className="text-[#111418] text-base font-medium leading-normal">
-                This token has a 1:1 mapping of the amount of Co2 emissions tons
-                that we can get away with to stay under the 2 degrees global
-                warming by 2050. Current estimates mean there are 1 trillion
-                tokens : 1 trillion mega tons of emissions to track and reduce
-                via the insuance of this token, similar to carbon credits. The
-                more tokens you earn, the greater impact we may have on this
-                seemingly impossible feat
-              </Text>
-            </View>
-            <View className="flex flex-col items-center gap-1 pb-3">
-              <View className="w-[1.5px] bg-[#dce0e5] h-4" />
-              <View className="size-2 rounded-full bg-[#111418]" />
-            </View>
-            <View className="flex flex-1 flex-col pt-3 pb-5">
-              <Text className="text-[#111418] text-base font-medium leading-normal">
-                This example of a web3 wallet in the chrome browser (a few
-                simple user friendly instructions later for those unware of the
-                basics of operating web3 wallets)
-              </Text>
+            <View className="flex flex-row items-start gap-x-2 w-full">
+              {/* Left Side: Dot and Line */}
+              <View className="flex items-center">
+                <View className="w-[1.5px] h-4 bg-[#dce0e5]" />
+                <View className="w-2 h-2 rounded-full bg-[#111418] my-1" />
+              </View>
+
+              {/* Right Side: Text */}
+              <View className="flex-1 pt-3 pb-5">
+                <Text className="text-[#111418] text-base font-medium leading-normal">
+                  This example of a web3 wallet in the chrome browser (a few
+                  simple user friendly instructions later for those unware of
+                  the basics of operating web3 wallets)
+                </Text>
+              </View>
             </View>
           </View>
-          <View className="flex flex-1 flex-col pl-16 pr-8 pb-10">
+          <View className="flex flex-1 flex-col pl-8 pr-8 pb-10">
             <Link href="/sign-up" asChild>
               <Button className="mt-2 bg-purple-500 py-2 px-4 rounded text-center">
                 <Text className="text-white ">Lets do this</Text>
